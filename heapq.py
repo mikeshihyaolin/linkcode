874edcf65992bbc6c656678c6f27b636260eb967
mikeshihyaolin <shihyao.ca@gmail.com>
2019-07-21 11:54:50 -0700

    heap fundamentals

---
+++ heapq.py
@@ -0,0 +1,32 @@
+# Python code to demonstrate working of  
+# heapify(), heappush() and heappop() 
+  
+# importing "heapq" to implement heap queue 
+import heapq 
+  
+# initializing list 
+li = [5, 7, 9, 1, 3] 
+
+print(li)
+  
+# using heapify to convert list into heap 
+heapq.heapify(li) 
+
+print(li)
+  
+# printing created heap 
+print ("The created heap is : ",end="") 
+print (list(li)) 
+  
+# using heappush() to push elements into heap 
+# pushes 4 
+heapq.heappush(li,4) 
+  
+# printing modified heap 
+print ("The modified heap after push is : ",end="") 
+print (list(li)) 
+  
+# using heappop() to pop smallest element 
+print ("The popped and smallest element is : ",end="") 
+print (heapq.heappop(li)) 
+print (list(li)) 
